5e674691617535df2f60bb8e58cbde1a880ed0d6
theijhay <olawaleisaacjohn@gmail.com>
2024-05-09 13:29:14 +0000

    SQL script that creates a stored procedure ComputeAverageScoreForUser

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,22 @@
-DELIMITER //
-
 -- Create stored procedure ComputeAverageScoreForUser
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
 BEGIN
-    DECLARE total_score FLOAT DEFAULT 0;
-    DECLARE total_count INT DEFAULT 0;
-    DECLARE avg_score FLOAT DEFAULT 0;
+    DECLARE avg_score FLOAT;
 
-    -- Calculate total score and count of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, total_count FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
 
-    -- Calculate average score
-    IF total_count > 0 THEN
-        SET avg_score = total_score / total_count;
-    END IF;
-
-    -- Update the user's average_score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END;
-
-//
+    UPDATE users
+    SET average_score = avg_score
+    WHERE users.id = user_id;
+END$$
 DELIMITER ;
 
